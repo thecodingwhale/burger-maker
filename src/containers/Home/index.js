84e1e2c32a708588a473e318656e5ce9cfe194a0
Aldren Terante <aldrenterante@altus-dc.com>
2018-05-06 01:53:56 +0800

    add lists of ingredients for burger

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import {
+  ADD_INGREDIENT
+} from './constants';
 
 class Home extends React.Component {
+  selectIngredient = (key) => {
+    this.props.onAddIngredient(key);
+  }
+
   render() {
     return (
-      <div>
-        Home
+      <div className="cutting-board">
+        <div className="cutting-board__ingredients">
+          <div className="button-group button-group--column">
+            {this.props.ingredients.map((ingredient, index) => (
+              <button
+                key={index}
+                className="button button--default"
+                onClick={() => this.selectIngredient(ingredient.key)}
+              >
+                {ingredient.name}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="cutting-board__main-area">
+
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = state => {
+  const { burger, ingredients } = state;
+  return {
+    ingredients,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddIngredient: (key) => dispatch({
+      type: ADD_INGREDIENT,
+      key,
+    }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
--- src/redux.js
+++ src/redux.js
@@ -1,11 +1,53 @@
 import update from 'immutability-helper';
 
-const initialState = {
+import { ADD_INGREDIENT } from './containers/Home/constants';
 
+const initialState = {
+  burger: {
+    ingredients: []
+  },
+  ingredients: [{
+    key: 1,
+    name: 'bun',
+  }, {
+    key: 2,
+    name: 'tomato',
+  }, {
+    key: 3,
+    name: 'salad',
+  }, {
+    key: 4,
+    name: 'patty',
+  }, {
+    key: 5,
+    name: 'bacon',
+  }, {
+    key: 6,
+    name: 'mushrooms',
+  }, {
+    key: 7,
+    name: 'cucumber',
+  }, {
+    key: 8,
+    name: 'egg',
+  }, {
+    key: 9,
+    name: 'onion',
+  }, {
+    key: 10,
+    name: 'jalapeno',
+  }],
 };
 
 export function reducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_INGREDIENT:
+      return update(state, {
+        burger: {
+          ingredients: {$push: [action.key]},
+        }
+      });
+    break;
     default:
       return state;
   }
